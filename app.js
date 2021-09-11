34a3331b185a7e57e0aadf992a859874da5d32d3
Abhinav T K <63438668+abhinavtk@users.noreply.github.com>
2021-09-11 17:03:17 +0530

    Update- all

---
+++ app.js
@@ -0,0 +1,91 @@
+const birthDate = document.querySelector("#birth-date");
+const checkBtn = document.querySelector('#check-btn');
+const output = document.querySelector("#output");
+
+function reverseStr(date){
+    var str = date;
+    var charList = str.split('');
+    var reversedList = charList.reverse();
+    reversedList.join('');
+}
+
+function checkPalindrome(date){
+    return date===reverseStr(date);  
+}
+
+function dateAsString(date){
+    var dateAsStr = {day: '', month: '', year: ''};
+  
+    if(date.day < 10){
+        dateAsStr.day = '0' + date.day;
+     }
+    else {
+        dateAsStr.day = date.day.toString();
+    }
+    
+    if(date.month < 10){
+        dateAsStr.month = '0' + date.month;
+    }
+    else {
+        dateAsStr.month = date.month.toString();
+    }
+    dateAsStr.year = date.year.toString();
+    return dateAsStr;
+  }
+
+  function getDateInAllFormats(date) {
+    var ddmmyyyy = date.day + date.month + date.year;
+    var mmddyyyy = date.month + date.day + date.year;
+    var yyyymmdd = date.year + date.month + date.day;
+    var ddmmyy = date.day + date.month + date.year.slice(-2);
+    var mmddyy = date.month + date.day + date.year.slice(-2);
+    var yyddmm = date.year.slice(-2) + date.day + date.month;
+  
+    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yyddmm];
+  }
+
+  function checkPalindromeForAllDateFormats(date) {
+    var dateFormatList = getDateInAllFormats(date);
+    var palindromeList = [];
+  
+    for (var i = 0; i < dateFormatList.length; i++) {
+      var result = checkPalindrome(dateFormatList[i]);
+      palindromeList.push(result);
+    }
+    return palindromeList;
+  }
+
+function clickHandler(){
+    var birthStr = birthDate.value;
+    if (birthStr != ''){
+        var date = birthStr.split('-');
+        var yyyy = date[0];
+        var mm = date[1];
+        var dd = date[2];
+
+        var dateObj = {
+        day: Number(dd),
+        month: Number(mm),
+        year: Number(yyyy)
+        };
+    }
+    var dateStr = dateAsString(dateObj);
+    var list = checkPalindromeForAllDateFormats(dateStr);
+    var isPalindrome = false;
+
+    for (let i = 0; i < list.length; i++) {
+    console.log(list[i]);
+      if (list[i]) {
+        isPalindrome = true;
+        break;
+      }
+    }
+    if (!isPalindrome){
+        output.innerText = "Not palindrome";
+    }
+    else{
+        output.innerText = "Palindrome";
+    }
+}
+
+checkBtn.addEventListener('click',clickHandler);
